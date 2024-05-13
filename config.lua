45c16651905fb4dda3c2db91763e8704543298a1
Atiya <matiyaaaa@gmail.com>
2024-05-13 23:30:52 +0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,16 +2,13 @@ Config = {}
 
 Config.Target = "OX" -- "QB", "OX"
 
-Config.AllPianos = true -- true = all pianos, false = only pianos specified in Config.PianoLocations
+Config.AllPianos = true -- true = all pianos, false = only pianos specified in Config.PianoLocations. If true, Config.PianoLocations will be used as spawning extra pianos
 
 Config.AudioDistance = "20.0"
 
-Config.PropName = {
-    "sf_prop_sf_piano_01a",
-    -- "prop_otherpiano_01a" (this doesn't exist, i made it up)
-}
+Config.PropName = "sf_prop_sf_piano_01a"
 
 Config.PianoLocations = {
-    { coords = vector4(686.44, 577.82, 129.46, 100.0) },
-    { coords = vector4(955.36, 37.09, 70.78, 0.00) }
+    "vector4(686.44, 577.82, 129.46, 100.0)",
+    "vector4(955.36, 37.09, 70.78, 0.00)",
 }
