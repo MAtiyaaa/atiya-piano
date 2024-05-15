8e2d63f0878b1f836661c9c14b39a255e78ee740
Atiya <matiyaaaa@gmail.com>
2024-05-15 18:18:04 +0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,25 +2,25 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'Atiya Piano'
-version '1.0.1'
+version '1.0.2'
 
 client_scripts {
     'client/*.lua'
 }
-
 server_scripts {
     'server/*.lua'
 }
 
 shared_scripts {
-    'config.lua'
+    'shared/*.lua'
 }
+
+dependency 'xsound'
 
 ui_page 'html/index.html'
 
 files {
     'html/index.html',
     'html/styles.css',
-    'html/script.js',
-    'html/notes/*.mp3',
+    'html/script.js'
 }
