7fe0f4f9f9dcb7730047c2f9d7d82b47a1a5d214
Atiya <matiyaaaa@gmail.com>
2024-05-15 18:18:22 +0400

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,8 +1,49 @@
-RegisterNetEvent('atiya-piano:PlayWithinDistance')
-AddEventHandler('atiya-piano:PlayWithinDistance', function(maxDistance, note, volume)
-    local src = source
-    local playerPed = GetPlayerPed(src)
-    local playerCoords = GetEntityCoords(playerPed)
+local propHash = GetHashKey(Config.PropName)
+local spawnedPianos = {}
 
-    TriggerClientEvent('atiya-piano:playSoundForNearby', -1, note, volume, playerCoords, src)
+RegisterNetEvent('atiya-piano:playNote')
+AddEventHandler('atiya-piano:playNote', function(note, volume, coords)
+    TriggerClientEvent('atiya-piano:playNote', -1, note, volume, coords)
+end)
+
+function loadServerPianos()
+    for _, pianoData in ipairs(Config.PianoLocations) do
+        if not spawnedPianos[pianoData.coords] then
+            spawnedPianos[pianoData.coords] = {coords = pianoData.coords, heading = pianoData.heading}
+            TriggerClientEvent('atiya-piano:spawnPiano', -1, pianoData.coords, pianoData.heading, propHash)
+            if Config.Debug then
+                print("Server: Piano data sent for spawning at:", pianoData.coords)
+            end
+        end
+    end
+end
+
+function deleteServerPianos()
+    for locStr, _ in pairs(spawnedPianos) do
+        TriggerClientEvent('atiya-piano:deletePiano', -1, locStr)
+        if Config.Debug then
+            print("Server: Piano delete signal sent for:", locStr)
+        end
+    end
+    spawnedPianos = {}
+end
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        loadServerPianos()
+    end
+end)
+
+AddEventHandler('onResourceStop', function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        deleteServerPianos()
+    end
+end)
+
+RegisterNetEvent('atiya-piano:requestPianos')
+AddEventHandler('atiya-piano:requestPianos', function()
+    local _source = source
+    for coords, pianoData in pairs(spawnedPianos) do
+        TriggerClientEvent('atiya-piano:spawnPiano', _source, coords, pianoData.heading, propHash)
+    end
 end)
