5edb799e400ffa2470021d30863bfacb37b4a01a
Atiya <matiyaaaa@gmail.com>
2024-05-15 18:16:08 +0400

    Update and rename config.lua to shared/config.lua

---
+++ shared/config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.Debug = false
+
+Config.Target = "OX" -- "QB", "OX"
+
+Config.AudioDistance = 35.0
+
+Config.PropName = "sf_prop_sf_piano_01a"
+
+Config.PianoLocations = { -- All piano props will work with this script, this config is only to spawn them in specific locations
+    { coords = "686.44, 577.82, 129.46, 100.0" },
+    { coords = "165.49, -985.57, 29.09, 105.00" },
+}
