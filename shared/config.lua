21c23e2b2e2f28512d5aa299fbd886661dea69c6
Atiya <matiyaaaa@gmail.com>
2024-05-16 03:34:17 +0400

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -8,6 +8,8 @@ Config.AudioDistance = 35.0
 
 Config.PropName = "sf_prop_sf_piano_01a"
 
+Config.BenchName = "sf_prop_sf_bench_piano_01a"
+
 Config.PianoLocations = { -- All piano props will work with this script, this config is only to spawn them in specific locations
     { coords = "686.44, 577.82, 129.46, 100.0" },
     { coords = "165.49, -985.57, 29.09, 105.00" },
